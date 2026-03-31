61862be1160a7db5dbe76b15a57a0f0ed2c3cd86
Teuku Atha Athaya Nafi <teukuatha19@gmail.com>
2026-03-31 22:21:15 +0700

    membuat struktur dan route teukuatha

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import Abdu from './pages/Abdu'
+  import { Routes, Route } from 'react-router-dom'
+  import Dashboard from './pages/Dashboard'
+  import Abdu from './pages/Abdu'
+  import TeukuAtha from './pages/TeukuAtha' 
+  function App() {
+    return (
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/abdu" element={<Abdu />} />
+        <Route path="/teuku-atha" element={<TeukuAtha />} />
+      </Routes>
+    )
+  }
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/abdu" element={<Abdu />} />
-    </Routes>
-  )
-}
-
-export default App
+  export default App
---
+++ src/pages/TeukuAtha.jsx
@@ -0,0 +1,7 @@
+export default function TeukuAtha() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-2xl font-bold">Portfolio Teuku Atha - Coming Soon</h1>
+    </div>
+  );
+}
